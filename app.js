1641a731f00f8083cc25f422d425470c7410d01a
CankutER <cnkt.er@gmail.com>
2022-10-08 17:59:58 +0300

    added removal and movedown of completed rows

--- app.js
+++ app.js
@@ -22,17 +22,16 @@ function play(time) {
 
   const delta = time - prevTime;
 
-  if (delta >= 500) {
+  if (delta >= 400) {
     game.draw();
     if (!game.ifCollides()) {
       game.update();
     }
+    game.ifRemove();
     prevTime = time;
   }
   window.requestAnimationFrame(play);
 }
-
-window.requestAnimationFrame(play);
 
 //
 
@@ -41,8 +40,24 @@ class Board {
   totalCol = 10;
   constructor(element) {
     this.element = element;
+    this.removeControl = false;
     this.prevActiveShape = [];
-    this.activeShape = [{ row: 1, col: 5 }];
+    this.prevPlaced = [];
+    this.beforeMoveDown = [];
+    this.activeShape = [
+      ...(function e() {
+        const arr = new Array(10).fill({}).map((element, i) => {
+          const temp = {};
+          temp.row = 2;
+          temp.col = i + 1;
+
+          return temp;
+        });
+
+        return arr;
+      })(),
+      { row: 1, col: 5 },
+    ];
     this.placed = [];
   }
   draw() {
@@ -56,6 +71,20 @@ class Board {
 
       shape.classList.add("playing");
     });
+    if (this.removeControl) {
+      this.prevPlaced.forEach((piece) => {
+        const shape = document.getElementById(`${piece.row}${piece.col}`);
+
+        shape.classList.remove("played");
+        shape.classList.remove("playing");
+      });
+      this.beforeMoveDown.forEach((piece) => {
+        const shape = document.getElementById(`${piece.row}${piece.col}`);
+
+        shape.classList.remove("played");
+        shape.classList.remove("playing");
+      });
+    }
     this.placed.forEach((piece) => {
       const shape = document.getElementById(`${piece.row}${piece.col}`);
       shape.classList.add("played");
@@ -65,17 +94,28 @@ class Board {
     this.prevActiveShape = this.activeShape.map((item) => {
       return { ...item };
     });
-    if (this.activeShape.every((item) => item.row < this.totalRow)) {
-      this.activeShape.forEach((piece) => {
-        piece.row += 1;
-      });
-    } else {
-      const temp = this.activeShape.map((item) => {
-        return { ...item };
-      });
-      this.placed.push(...temp);
-      this.activeShape = [{ row: 1, col: 5 }];
-    }
+    this.activeShape.forEach((piece) => {
+      piece.row += 1;
+    });
+
+    // REDUNDANT OLD CODE
+
+    // this.prevActiveShape = this.activeShape.map((item) => {
+    //   return { ...item };
+    // });
+    // if (this.activeShape.every((item) => item.row < this.totalRow)) {
+    //   this.activeShape.forEach((piece) => {
+    //     piece.row += 1;
+    //   });
+    // } else {
+    //   const temp = this.activeShape.map((item) => {
+    //     return { ...item };
+    //   });
+    //   this.placed.push(...temp);
+    //   this.activeShape = [{ row: 1, col: 5 }];
+    // }
+
+    //
   }
   ifCollides() {
     let control = false;
@@ -96,10 +136,96 @@ class Board {
         return { ...item };
       });
       this.placed.push(...temp);
-      this.activeShape = [{ row: 1, col: 5 }];
+      this.activeShape = [
+        ...(function e() {
+          const arr = new Array(10).fill({}).map((element, i) => {
+            const temp = {};
+            temp.row = 2;
+            temp.col = i + 1;
+
+            return temp;
+          });
+
+          return arr;
+        })(),
+        { row: 1, col: 5 },
+      ];
     }
     return control;
+  }
+  ifRemove() {
+    // find how many slots are filled in each row
+    const rowStats = this.placed.reduce((stats, curr) => {
+      if (stats[curr.row]) {
+        stats[curr.row] += 1;
+      } else {
+        stats[curr.row] = 1;
+      }
+      return stats;
+    }, {});
+    //
+
+    // mark rows to be removed
+    this.removeControl = false;
+    for (const row in rowStats) {
+      if (rowStats[row] === this.totalCol) {
+        this.removeControl = true;
+
+        this.prevPlaced = this.placed.map((slot) => {
+          return { ...slot };
+        });
+
+        this.placed.forEach((slot, i) => {
+          if (slot.row == row) {
+            this.placed[i] = "toBeRemoved";
+          }
+        });
+      }
+    }
+    //
+
+    // remove marked rows
+    if (this.removeControl) {
+      const filteredPlaced = this.placed.filter(
+        (slot) => slot !== "toBeRemoved"
+      );
+      this.placed = filteredPlaced.map((item) => {
+        return { ...item };
+      });
+    }
+    //
+
+    // finding which rows to be deleted
+    const findRowsToDelete = () => {
+      const tempSet = new Set();
+      for (const row in rowStats) {
+        if (rowStats[row] == this.totalCol) {
+          tempSet.add(row);
+        }
+      }
+      return tempSet;
+    };
+
+    const rowsToBeDeleted = findRowsToDelete();
+    //
+
+    // Moving down after removal
+    this.beforeMoveDown = this.placed.map((item) => {
+      return { ...item };
+    });
+    rowsToBeDeleted.forEach((row) => {
+      this.beforeMoveDown.forEach((slot, i) => {
+        if (slot.row < row) {
+          if (this.placed[i]) {
+            this.placed[i].row = Number(slot.row) + 1;
+          }
+        }
+      });
+    });
+    //
   }
 }
 
 const game = new Board(board);
+
+window.requestAnimationFrame(play);
